09d9bbc39f3740c8d870d1b1626ee0a3b351e32a
khushbu-14 <thakur.k@northeaster.edu>
2021-06-23 21:12:26 -0400

    adding star prototype

--- src/edu/neu/csye7374/prototypes/AStarProtoype.java
+++ src/edu/neu/csye7374/prototypes/AStarProtoype.java
@@ -1,5 +1,18 @@
 package edu.neu.csye7374.prototypes;
 
 public class AStarProtoype {
+	
+	@Override
+	public void showMagnitude(int value) {
+		String str = "*".repeat(value);
+		System.out.println("["+value + "] " + str);
+	}
 
+	@Override
+	public void showMagnitude(double value) {
+		// TODO Auto-generated method stub
+		int val = value;
+		showMagnitude(val);
+	}
+	
 }
