a08b7a55262285a6eb3ba7ec7978de7d0fbc30e3
andrewhu0209 <54827860+andrewhu0209@users.noreply.github.com>
2021-06-23 21:19:22 -0400

    add Adot, AnX impl

--- src/edu/neu/csye7374/prototypes/ADotPrototype.java
+++ src/edu/neu/csye7374/prototypes/ADotPrototype.java
@@ -1,5 +1,30 @@
 package edu.neu.csye7374.prototypes;
 
-public class ADotPrototype {
+import edu.neu.csye7374.api.GraphPrototypeAPI;
+
+public class ADotPrototype extends GraphPrototypeAPI {
+
+    @Override
+    public void showMagnitude(int value) {
+        // TODO Auto-generated method stub
+        StringBuffer buf = new StringBuffer();
+        buf.append("[" + value + "]");
+        for (int i = 0; i < value; i++) {
+            buf.append(".");
+        }
+        System.out.println(buf.toString());
+    }
+
+    @Override
+    public void showMagnitude(double value) {
+        // TODO Auto-generated method stub
+        StringBuffer buf = new StringBuffer();
+        buf.append("[" + value + "]");
+        for (double i = 0; i < Math.round(value); i++) {
+            buf.append(".");
+        }
+        System.out.println(buf.toString());
+
+    }
 
 }
