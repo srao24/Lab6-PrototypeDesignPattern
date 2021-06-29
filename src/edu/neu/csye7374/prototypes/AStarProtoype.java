0fab5bfcaa68e29f1391372b2793da77fffdd75f
rao.sid <rao.sid@WFXA44CC82F17AA.nunet.neu.edu>
2021-06-29 18:12:35 -0400

    ShowMagnitude(double) fixed

--- src/edu/neu/csye7374/prototypes/AStarProtoype.java
+++ src/edu/neu/csye7374/prototypes/AStarProtoype.java
@@ -7,14 +7,21 @@ public class AStarProtoype extends GraphPrototypeAPI {
 	@Override
 	public void showMagnitude(int value) {
 		String str = "*".repeat(value);
-		System.out.println("["+value + "] " + str);
+		System.out.println("["+value + "]   " + str);
 	}
 
 	@Override
 	public void showMagnitude(double value) {
 		// TODO Auto-generated method stub
-		int val = (int)value;
-		showMagnitude(val);
+//		int val = (int)value;
+//		showMagnitude(val);
+		StringBuilder sb = new StringBuilder();
+		sb.append("["+value+"]:");
+		for(int i=0;i<Math.round(value);i++) {
+			sb.append("*");
+		}
+		
+	System.out.println(sb.toString());
 	}
 	
 }
